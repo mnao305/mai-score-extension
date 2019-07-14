3c99de7419f522d5d8e2905e98d9d529cc3a2ec1
mnao305 <naomasa305@gmail.com>
2019-07-14 21:09:19 +0900

    add: Firestoreへのスコアデータを登録

--- src/popup/router/pages/Index.vue
+++ src/popup/router/pages/Index.vue
@@ -2,21 +2,27 @@
   <div id="popupIndex">
     <p>舞スコア データ取得ツール</p>
     <button @click="getData">データ取得</button>
+    <p v-if="message">{{ message }}</p>
   </div>
 </template>
 
 <script>
 // import Browser from 'webextension-polyfill'
 import Axios from 'axios'
+import { db } from '../../plugins/firestore'
 export default {
   data() {
-    return {}
+    return {
+      message: '',
+    }
   },
   methods: {
     async getData() {
       const difficultyLevel = ['basic', 'advanced', 'expert', 'master', 'remaster']
       let scoreData = {}
+      let scoreDataDX = {}
       for (let i = 0; i < difficultyLevel.length; i++) {
+        this.message = `${difficultyLevel[i]}データを読み込み中...`
         try {
           const { data } = await Axios.get(`https://maimaidx.jp/maimai-mobile/record/musicGenre/search/?genre=99&diff=${i}`)
           const tmpEl = document.createElement('div')
@@ -24,26 +30,62 @@ export default {
           const classList = tmpEl.getElementsByClassName('w_450 m_15 p_r f_0')
 
           let scoreList = {}
+          let scoreListDX = {}
           for (let i = 0; i < classList.length; i++) {
             const tmp = classList[i].innerText
               .trim()
               .split('\n')
               .map(v => v.trim())
-            scoreList[tmp[1]] = {
-              level: tmp[0],
-              title: tmp[1],
-              achievement: tmp[2] ? tmp[2] : null,
-              dxScore: tmp[3] ? tmp[3] : null,
-              type: classList[i].lastElementChild.src.indexOf('standard.png') >= 0 ? 'standard' : 'deluxe',
+
+            if (classList[i].lastElementChild.src.indexOf('standard.png') >= 0) {
+              scoreList[tmp[1]] = {
+                title: tmp[1],
+                achievement: tmp[2] ? tmp[2] : null,
+                dxScore: tmp[3] ? tmp[3] : null,
+                type: 'standard',
+              }
+            } else {
+              scoreListDX[tmp[1]] = {
+                title: tmp[1],
+                achievement: tmp[2] ? tmp[2] : null,
+                dxScore: tmp[3] ? tmp[3] : null,
+                type: 'deluxe',
+              }
             }
           }
           scoreData[difficultyLevel[i]] = scoreList
+          scoreDataDX[difficultyLevel[i]] = scoreListDX
         } catch (error) {
           console.error(error)
           continue
         }
       }
       console.log(scoreData)
+      console.log(scoreDataDX)
+      this.message = 'データ保存中...'
+      for (let i = 0; i < difficultyLevel.length; i++) {
+        db.collection('users')
+          .doc('l6aWFHMbrNhDnvLsHsrH')
+          .collection('scores')
+          .doc(Object.keys(scoreData)[i])
+          .collection('standard')
+          .doc('list')
+          .set(scoreData[Object.keys(scoreData)[i]], { merge: true })
+          .catch(e => {
+            console.error(e)
+          })
+        db.collection('users')
+          .doc('l6aWFHMbrNhDnvLsHsrH')
+          .collection('scores')
+          .doc(Object.keys(scoreDataDX)[i])
+          .collection('deluxe')
+          .doc('list')
+          .set(scoreDataDX[Object.keys(scoreDataDX)[i]], { merge: true })
+          .catch(e => {
+            console.error(e)
+          })
+      }
+      this.message = 'データ保存完了！'
     },
   },
 }
