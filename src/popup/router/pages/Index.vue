32ae212e1f86ea0b3b2165155d6a15b843073831
mnao305 <naomasa305@gmail.com>
2019-07-15 09:43:31 +0900

    update: ランク情報等を追加

--- src/popup/router/pages/Index.vue
+++ src/popup/router/pages/Index.vue
@@ -42,6 +42,54 @@ export default {
               .split('\n')
               .map(v => v.trim())
 
+            const imgList = classList[j].getElementsByClassName('h_30 f_r')
+            let rank = null
+            let comboRank = null
+            let sync = null
+            for (let k = 0; k < imgList.length; k++) {
+              if (imgList[k].src.indexOf('_fc.png') >= 0) {
+                comboRank = 'FC'
+              } else if (imgList[k].src.indexOf('_fcp.png') >= 0) {
+                comboRank = 'FC+'
+              } else if (imgList[k].src.indexOf('_ap.png') >= 0) {
+                comboRank = 'AP'
+              } else if (imgList[k].src.indexOf('_app.png') >= 0) {
+                comboRank = 'AP+'
+              } else if (imgList[k].src.indexOf('_d.png') >= 0) {
+                rank = 'D'
+              } else if (imgList[k].src.indexOf('_c.png') >= 0) {
+                rank = 'C'
+              } else if (imgList[k].src.indexOf('_b.png') >= 0) {
+                rank = 'B'
+              } else if (imgList[k].src.indexOf('_bb.png') >= 0) {
+                rank = 'BB'
+              } else if (imgList[k].src.indexOf('_bbb.png') >= 0) {
+                rank = 'BBB'
+              } else if (imgList[k].src.indexOf('_a.png') >= 0) {
+                rank = 'A'
+              } else if (imgList[k].src.indexOf('_aa.png') >= 0) {
+                rank = 'AA'
+              } else if (imgList[k].src.indexOf('_aaa.png') >= 0) {
+                rank = 'AAA'
+              } else if (imgList[k].src.indexOf('_s.png') >= 0) {
+                rank = 'S'
+              } else if (imgList[k].src.indexOf('_ss.png') >= 0) {
+                rank = 'SS'
+              } else if (imgList[k].src.indexOf('_sss.png') >= 0) {
+                rank = 'SSS'
+              } else if (imgList[k].src.indexOf('_sp.png') >= 0) {
+                rank = 'S+'
+              } else if (imgList[k].src.indexOf('_ssp.png') >= 0) {
+                rank = 'SS+'
+              } else if (imgList[k].src.indexOf('_sssp.png') >= 0) {
+                rank = 'SSS+'
+              } else if (imgList[k].src.indexOf('_fs.png') >= 0) {
+                sync = 'FS'
+              } else if (imgList[k].src.indexOf('_fsd.png') >= 0) {
+                sync = 'FDX'
+              }
+            }
+
             const type = classList[j].lastElementChild.src.indexOf('standard.png') >= 0 ? 'standard' : 'deluxe'
             scoreData[difficultyLevel[i]][`${tmp[1]}_${difficultyLevel[i]}_${type}`] = {
               title: tmp[1],
@@ -51,6 +99,9 @@ export default {
               genre: genre,
               difficultyLevel: difficultyLevel[i],
               level: tmp[0].indexOf('+') >= 0 ? Number(tmp[0].replace('+', '')) + 0.5 : Number(tmp[0].replace('+', '')),
+              rank: rank,
+              comboRank: comboRank,
+              sync: sync,
             }
           }
         } catch (error) {
