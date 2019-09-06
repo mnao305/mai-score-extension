38bd2b32ec916e6d0224c01e99c1a1f62b5e9e9c
mnao305 <naomasa305@gmail.com>
2019-09-07 07:57:32 +0900

    musicIDが可変するのでハッシュ生成元の変更

--- src/popup/router/pages/components/AddScoreData.vue
+++ src/popup/router/pages/components/AddScoreData.vue
@@ -101,11 +101,12 @@ export default {
               .split('\n')
               .map(v => v.trim())
 
-            // 曲IDからハッシュ値を取って、DBの保存時に使う。
-            const shaObj = new JsSHA('SHA-1', 'B64')
-            const musicID = classList[j].getElementsByTagName('input')[0].value
-            shaObj.update(musicID)
-            const musicIDHash = shaObj.getHash('HEX')
+            const type = classList[j].lastElementChild.src.indexOf('standard.png') >= 0 ? 'standard' : 'deluxe'
+
+            // 曲名などからハッシュ値を取って、DBの保存時に使う。
+            const shaObj = new JsSHA('SHA-1', 'TEXT')
+            shaObj.update(`${tmp[1]}_${difficultyLevel[i]}_${type}_${genre}`)
+            const musicHash = shaObj.getHash('HEX')
 
             const imgList = classList[j].getElementsByClassName('h_30 f_r')
             let rank = null
@@ -161,13 +162,12 @@ export default {
 
             let musicUpdateDate
 
-            const type = classList[j].lastElementChild.src.indexOf('standard.png') >= 0 ? 'standard' : 'deluxe'
             let oldAchievement = []
             let oldDxScore = []
-            if (gotOldScore && gotOldScore[musicIDHash]) {
-              oldAchievement = gotOldScore[musicIDHash].achievements || []
-              oldDxScore = gotOldScore[musicIDHash].dxScores || []
-              musicUpdateDate = gotOldScore[musicIDHash].date || date
+            if (gotOldScore && gotOldScore[musicHash]) {
+              oldAchievement = gotOldScore[musicHash].achievements || []
+              oldDxScore = gotOldScore[musicHash].dxScores || []
+              musicUpdateDate = gotOldScore[musicHash].date || date
             } else {
               musicUpdateDate = date
             }
@@ -192,7 +192,7 @@ export default {
 
             const achievements = tmp[2] ? oldAchievement : null
             const dxScores = tmp[3] ? oldDxScore : null
-            scoreData[difficultyLevel[i]][musicIDHash] = {
+            scoreData[difficultyLevel[i]][musicHash] = {
               title: tmp[1],
               achievements: achievements,
               dxScores: dxScores,
@@ -204,10 +204,10 @@ export default {
               comboRank: comboRank,
               sync: sync,
               date: musicUpdateDate,
-              musicID: musicID,
+              musicID: classList[j].getElementsByTagName('input')[0].value,
             }
             if (updateFlg) {
-              updateScoreData.push(scoreData[difficultyLevel[i]][musicIDHash])
+              updateScoreData.push(scoreData[difficultyLevel[i]][musicHash])
             }
           }
         } catch (error) {
